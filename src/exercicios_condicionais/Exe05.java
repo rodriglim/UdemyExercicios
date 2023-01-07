0948d6827bf77f4dd0d7c9fb5cb52b3f25990bbe
Rodrigo Lima <109808618+rodriglim@users.noreply.github.com>
2023-01-06 21:23:56 -0300

    Add files via upload

---
+++ src/exercicios_condicionais/Exe05.java
@@ -0,0 +1,32 @@
+package exercicios_condicionais;
+
+import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
+
+public class Exe05 {
+
+    public static void main(String[] args) {
+
+        int codigoProduto, quantidadeProduto;
+     
+
+        String[][] codigo = new String[][]{{"1", "2", "3", "4", "5"}, {"Cachorro Quente", "X-Salada", "X-Bacon", "Torrada Simples", "Refrigerante"}};
+        float[] precoComida = new float[]{4.00f, 4.50f, 5.00f, 2.00f, 1.50f};
+        
+        try {
+            codigoProduto = Integer.parseInt(JOptionPane.showInputDialog("Informe o código do produto: ")) - 1;
+            if (codigoProduto < 0 || codigoProduto > 4) {
+                JOptionPane.showMessageDialog(null, "Código não existe");
+            } else {
+                quantidadeProduto = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a quantidade do produto: "));
+                double valorFinal = precoComida[codigoProduto] * quantidadeProduto;
+                String valorFormat = String.format("%.2f", valorFinal);
+                JOptionPane.showMessageDialog(null, "Total:R$ " + valorFormat);
+            }
+             
+            
+        } catch (HeadlessException | NumberFormatException e) {
+            
+        }
+    }
+}
